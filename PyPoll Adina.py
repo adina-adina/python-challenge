1b9091f87503136a031b71121a9f9ca04f97d0b8
adina-adina <adina77799@gmail.com>
2023-03-30 16:32:56 -0700

    Update PyPoll Adina.py

--- PyPoll Adina.py
+++ PyPoll Adina.py
@@ -9,3 +9,10 @@ total_votes = 0
 candidates = {}
 winner = ""
 max_votes = 0
+
+# Open and read "election_data.csv"
+with open(poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip header row
+    next(csvreader)
