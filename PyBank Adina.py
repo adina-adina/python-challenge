35cc6bc2d436f339b2df76046a048f1efe9ea207
adina-adina <adina77799@gmail.com>
2023-03-29 21:17:55 -0700

    Update PyBank Adina.py

--- PyBank Adina.py
+++ PyBank Adina.py
@@ -16,3 +16,20 @@ profit_change_list = []
 # Open and read the "budget_data" CSV file
 with open(census_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    # skip header row
+    next(csvreader)
+
+    # Loop through each row in "budget_data" and analyze data
+    for row in csvreader:
+
+        # count the total months
+        total_months += 1
+
+        # calculate the total profit
+        total_profit += int(row[1])
+
+        # calculate the total profit
+        profit_change += int(row[1]) - prev_profit
+        prev_profit = int(row[1])
+                        
