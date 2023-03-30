194cc1d35caa16adc219587feb0fd3d77208feb9
adina-adina <adina77799@gmail.com>
2023-03-29 20:34:56 -0700

    Update PyBank Adina.py

--- PyBank Adina.py
+++ PyBank Adina.py
@@ -5,3 +5,11 @@ import csv
 # define file path
 budget_csv = os.path.join("budget_data.csv")
 
+# define variables
+total_months = 0
+total_profit = 0
+greatest_increase = 0
+greatest_decrease = 0
+prev_profit = 0
+profit_change_list = []
+
